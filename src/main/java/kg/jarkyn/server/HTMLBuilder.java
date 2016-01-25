4965d314c949f99b9c57da190e156ce2eda297b6
jarkyn <jarkyn@ymail.com>
2016-01-25 06:52:13 +0000

    builds link with HTMLBuilder

---
+++ src/main/java/kg/jarkyn/server/HTMLBuilder.java
@@ -0,0 +1,9 @@
+package kg.jarkyn.server;
+
+public class HTMLBuilder {
+    private static String linkElementTemplate = "<a href=\"%s\">%s</a>";
+
+    public static String makeLink(String href, String linkText) {
+        return String.format(linkElementTemplate, href, linkText);
+    }
+}
---
+++ src/test/java/kg/jarkyn/server/HTMLBuilderTest.java
@@ -0,0 +1,15 @@
+package kg.jarkyn.server;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HTMLBuilderTest {
+
+    @Test
+    public void makesLink() {
+        String link = "<a href=\"/file1\">file1</a>";
+
+        assertEquals(link, HTMLBuilder.makeLink("/file1", "file1"));
+    }
+}
