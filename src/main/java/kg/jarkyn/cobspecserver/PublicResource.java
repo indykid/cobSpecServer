8564436491d7d4a74f5b74d05687bc03b4d9d485
jarkyn <jarkyn@ymail.com>
2016-02-05 10:09:19 +0000

    Handles access to public resources

---
+++ src/main/java/kg/jarkyn/cobspecserver/PublicResource.java
@@ -0,0 +1,43 @@
+package kg.jarkyn.cobspecserver;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+
+public class PublicResource {
+    private final String path;
+
+    public PublicResource(String path) {
+        this.path = path;
+    }
+
+    public String fullPathFor(String requestPath) {
+        return path + requestPath;
+    }
+
+    public boolean contains(String requestPath) {
+        return Files.exists(new File(fullPathFor(requestPath)).toPath());
+    }
+
+    public boolean isDirectory(String requestPath) {
+        return new File(fullPathFor(requestPath)).isDirectory();
+    }
+
+    public byte[] readFile(String requestPath) {
+        String fullPath = fullPathFor(requestPath);
+        if (contains(requestPath)) {
+            try {
+                return Files.readAllBytes((new File(fullPath)).toPath());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return new byte[0];
+    }
+
+    public List<String> readDirectory(String requestPath) {
+        return Arrays.asList(new File(fullPathFor(requestPath)).list());
+    }
+}
---
+++ src/test/java/kg/jarkyn/cobspecserver/DirectoryFixture.java
@@ -0,0 +1,21 @@
+package kg.jarkyn.cobspecserver;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class DirectoryFixture {
+    private static String projectPath = System.getProperty("user.dir");
+    public static String path = projectPath + "/src/test/resources/public";
+    public static String file1Path = path + "/file1";
+
+    public static byte[] fileByteContent() {
+        Path file = new File(file1Path).toPath();
+        try {
+            return Files.readAllBytes(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
