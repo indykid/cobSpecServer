7972b421328eca7e8874d6f5f2a6afe000199d13
jarkyn <jarkyn@ymail.com>
2016-02-05 09:27:17 +0000

    Parses request line

---
+++ src/test/java/kg/jarkyn/cobspecserver/RequestParserTest.java
@@ -0,0 +1,35 @@
+package kg.jarkyn.cobspecserver;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class RequestParserTest {
+    private final String input = "POST /?AuthId=SOMEKEY&Action=SomeAction HTTP/1.1\r\n" +
+                                 "Connection: keep-alive\r\n" +
+                                 "Accept-Language: en-US,en\r\n" +
+                                 "Accept: text/html\r\n\r\n" +
+                                 "body";
+    private final ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+    private RequestParser parser;
+    private Request request;
+
+    @Before
+    public void setUp() throws Exception {
+        parser = new RequestParser();
+        request = parser.parse(inputStream);
+    }
+
+    @Test
+    public void extractsMethod() {
+        assertEquals("POST", request.getMethod());
+    }
+
+    @Test
+    public void extractsPath() {
+        assertEquals("/", request.getPath());
+    }
+}
