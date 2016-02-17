a6b35d1cb1fea3ca44785aec221a0de3a9b47e7a
jarkyn <jarkyn@ymail.com>
2016-02-17 18:40:13 +0000

    Reads lines and characters
    - extends StreamHandler functionality

--- src/main/java/kg/jarkyn/cobspecserver/utils/StreamHandler.java
+++ src/main/java/kg/jarkyn/cobspecserver/utils/StreamHandler.java
@@ -1,12 +1,53 @@
 package kg.jarkyn.cobspecserver.utils;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StreamHandler {
     public static void write(OutputStream outputStream, byte[] message) {
         try {
             outputStream.write(message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<String> readUntilEmptyLine(BufferedReader reader) {
+        List<String> lines = new ArrayList<>();
+        String line;
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()) {
+                    break;
+                }
+                lines.add(line);
+            }
+            return lines;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String readLine(BufferedReader reader) {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static BufferedReader setupReader(InputStream inputStream) {
+        return new BufferedReader(new InputStreamReader(inputStream));
+    }
+
+    public static char[] readCharacters(BufferedReader reader, int length) {
+        char[] buffer = new char[length];
+        try {
+            reader.read(buffer);
+            return buffer;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/test/java/kg/jarkyn/cobspecserver/utils/StreamHandlerTest.java
+++ src/test/java/kg/jarkyn/cobspecserver/utils/StreamHandlerTest.java
@@ -2,9 +2,13 @@ package kg.jarkyn.cobspecserver.utils;
 
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StreamHandlerTest {
     @Test
@@ -14,5 +18,40 @@ public class StreamHandlerTest {
         StreamHandler.write(outputStream, "message".getBytes());
 
         assertEquals("message", outputStream.toString());
+    }
+
+    @Test
+    public void returnsReader() {
+        assertTrue(StreamHandler.setupReader(setupInputStream("irrelevant")) instanceof BufferedReader);
+    }
+
+    @Test
+    public void readsSingleLine() {
+        assertEquals("line1", StreamHandler.readLine(setupReader("line1\r\n" +
+                                                                 "line2\r\n")));
+    }
+
+    @Test
+    public void readsUntilEmptyLine() {
+        List<String> expectedLines = Arrays.asList("line1", "line2");
+
+        assertEquals(expectedLines,
+                     StreamHandler.readUntilEmptyLine(setupReader("line1\r\n" +
+                                                                  "line2\r\n" +
+                                                                  "\r\n" +
+                                                                  "body")));
+    }
+
+    @Test
+    public void readsCharacters() {
+        assertArrayEquals("body".toCharArray(), StreamHandler.readCharacters(setupReader("body"), 4));
+    }
+
+    private BufferedReader setupReader(String content) {
+        return new BufferedReader(new InputStreamReader(setupInputStream(content)));
+    }
+
+    private InputStream setupInputStream(String content) {
+        return new ByteArrayInputStream((content).getBytes());
     }
 }
